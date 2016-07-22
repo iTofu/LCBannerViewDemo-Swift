c6c42c8aefa12b96d8107bd271e5e230500dd51b
Leo <devtip@163.com>
2016-07-22 21:10:21 +0800

    Fix error.

--- LCBannerViewDemoi-Swift/ViewController.swift
+++ LCBannerViewDemoi-Swift/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 import LCBannerView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LCBannerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let bannerView = LCBannerView.init(frame: CGRectMake(0, 0, 200, 100), delegate: self, imageName: "AddImageByYouself", count: 3, timeInterval: 3.0, currentPageIndicatorTintColor: UIColor.whiteColor(), pageIndicatorTintColor: UIColor.grayColor())
+        let bannerView = LCBannerView.init(frame: CGRectMake(0, 0, 200, 100), delegate: self, imageName: "AddImageByYouself", count: 3, timeInterval: 3, currentPageIndicatorTintColor: UIColor.whiteColor(), pageIndicatorTintColor: UIColor.grayColor())
         
         view.addSubview(bannerView)
     }
